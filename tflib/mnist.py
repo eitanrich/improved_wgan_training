dfd3adfe0d403b78eb9a9b7cb63cce018a76522a
eitanrich <eitan.richardson@gmail.com>
2019-01-06 14:22:34 +0200

    WGAN-GP training of MNIST - separate model per class (digit)

--- tflib/mnist.py
+++ tflib/mnist.py
@@ -3,22 +3,24 @@ import numpy
 import os
 import urllib
 import gzip
-import cPickle as pickle
+import pickle
 
-def mnist_generator(data, batch_size, n_labelled, limit=None):
+def mnist_generator(data, batch_size, digits_filter, limit=None):
     images, targets = data
 
+    if digits_filter is not None:
+        relevant_samples = numpy.isin(targets, digits_filter)
+        images = images[relevant_samples]
+        targets = targets[relevant_samples]
+    print('Samples shape =', images.shape)
     rng_state = numpy.random.get_state()
     numpy.random.shuffle(images)
     numpy.random.set_state(rng_state)
     numpy.random.shuffle(targets)
     if limit is not None:
-        print "WARNING ONLY FIRST {} MNIST DIGITS".format(limit)
+        print("WARNING ONLY FIRST {} MNIST DIGITS".format(limit))
         images = images.astype('float32')[:limit]
         targets = targets.astype('int32')[:limit]
-    if n_labelled is not None:
-        labelled = numpy.zeros(len(images), dtype='int32')
-        labelled[:n_labelled] = 1
 
     def get_epoch():
         rng_state = numpy.random.get_state()
@@ -26,39 +28,28 @@ def mnist_generator(data, batch_size, n_labelled, limit=None):
         numpy.random.set_state(rng_state)
         numpy.random.shuffle(targets)
 
-        if n_labelled is not None:
-            numpy.random.set_state(rng_state)
-            numpy.random.shuffle(labelled)
+        num_samples = images.shape[0] - images.shape[0]%batch_size
+        image_batches = images[:num_samples].reshape(-1, batch_size, 784)
+        target_batches = targets[:num_samples].reshape(-1, batch_size)
 
-        image_batches = images.reshape(-1, batch_size, 784)
-        target_batches = targets.reshape(-1, batch_size)
-
-        if n_labelled is not None:
-            labelled_batches = labelled.reshape(-1, batch_size)
-
-            for i in xrange(len(image_batches)):
-                yield (numpy.copy(image_batches[i]), numpy.copy(target_batches[i]), numpy.copy(labelled))
-
-        else:
-
-            for i in xrange(len(image_batches)):
-                yield (numpy.copy(image_batches[i]), numpy.copy(target_batches[i]))
+        for i in xrange(len(image_batches)):
+            yield (numpy.copy(image_batches[i]), numpy.copy(target_batches[i]))
 
     return get_epoch
 
-def load(batch_size, test_batch_size, n_labelled=None):
+def load(batch_size, test_batch_size, digits_filter=None):
     filepath = '/tmp/mnist.pkl.gz'
     url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
 
     if not os.path.isfile(filepath):
-        print "Couldn't find MNIST dataset in /tmp, downloading..."
+        print("Couldn't find MNIST dataset in /tmp, downloading...")
         urllib.urlretrieve(url, filepath)
 
     with gzip.open('/tmp/mnist.pkl.gz', 'rb') as f:
         train_data, dev_data, test_data = pickle.load(f)
 
     return (
-        mnist_generator(train_data, batch_size, n_labelled), 
-        mnist_generator(dev_data, test_batch_size, n_labelled), 
-        mnist_generator(test_data, test_batch_size, n_labelled)
+        mnist_generator(train_data, batch_size, digits_filter),
+        mnist_generator(dev_data, test_batch_size, digits_filter),
+        mnist_generator(test_data, test_batch_size, digits_filter)
     )
--- tflib/plot.py
+++ tflib/plot.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 import matplotlib
@@ -19,7 +20,7 @@ def tick():
 def plot(name, value):
 	_since_last_flush[name][_iter[0]] = value
 
-def flush():
+def flush(out_folder='./'):
 	prints = []
 
 	for name, vals in _since_last_flush.items():
@@ -33,10 +34,10 @@ def flush():
 		plt.plot(x_vals, y_vals)
 		plt.xlabel('iteration')
 		plt.ylabel(name)
-		plt.savefig(name.replace(' ', '_')+'.jpg')
+		plt.savefig(os.path.join(out_folder, name.replace(' ', '_')+'.jpg'))
 
 	print("iter {}\t{}".format(_iter[0], "\t".join(prints)))
 	_since_last_flush.clear()
 
-	with open('log.pkl', 'wb') as f:
+	with open(os.path.join(out_folder, 'log.pkl'), 'wb') as f:
 		pickle.dump(dict(_since_beginning), f, pickle.HIGHEST_PROTOCOL)
